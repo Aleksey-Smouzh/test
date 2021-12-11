64c82b1dd2652cf910a58f71995a578c5e41d705
Aleksey-Smouzh <xmz@tut.by>
2021-12-11 20:50:30 +0300

    radio.buttom

--- script.js
+++ script.js
@@ -113,11 +113,11 @@
 // }
 // document.querySelector('.out-2').innerHTML = out1;
 //max
-// 
+//
 
 //________________cycle________________________________
 
-//for (старт; работаем_пока_верно, как_изменяется_счетчик,) 
+//for (старт; работаем_пока_верно, как_изменяется_счетчик,)
 // for (let i = 0; i < 5; i++){
 //   console.log(i);
 
@@ -129,11 +129,40 @@
 //   if (i == 4 ) break;
 // }
 
-let div = document.querySelectorAll('div');
+let div = document.querySelectorAll("div");
 console.log(div);
 //div.style.backgroundColor = 'red';
-for (let i = 0; i < div.length; i++){
-div[i].style.background = 'blue'
-console.log(div[i])
-}
+// for (let i = 0; i < div.length; i++) {
+//   div[i].style.background = "blue";
+//   div[i].style.color = "#ffff";
+//   div[i].style.marginBottom = '30px'
+//   console.log(div[i]);
+//   div[i].onclick = two;
+// }
 
+// function two() {
+//   console.log("work!!!");
+// }
+
+// let b = document.getElementsByClassName('one');
+// console.log(b);
+// for(let i = 0; i < b.length; i++){
+//   b[i].style.border = '5px solid black'
+// }
+
+document.querySelector("button").onclick = () => {
+  let r = document.querySelectorAll('input[type="radio"]');
+  console.log(r);
+  for (let i = 0; i < r.length; i++) {
+    if (r[i].checked) {
+      console.log(r[i].value);
+    }
+  }
+};
+
+
+let out = '';
+for (let i = 0; i < 10; i++){
+  out += i + ' ';
+}
+document.querySelector('out-1').innerHTML = "out";
