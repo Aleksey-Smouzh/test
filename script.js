4645abbc5b718f1b058d77a42018a2c0c9ab2b56
Aleksey-Smouzh <xmz@tut.by>
2021-12-11 17:43:01 +0300

    fund the max array

--- script.js
+++ script.js
@@ -86,23 +86,39 @@
 // console.log(man[4]);
 // man[4] = "sergeq";
 // console.log(man[4]);
-const b = document.createElement('span');
-b.className = "tets";
-b.style.fontWeight = '600';
-b.style.fontSize = "25px"
-document.body.append(b)
-let a = [1, 2, 3, 4];
-let t = a[0];
-a[0] = a[a.length -1];
-a[a.length -1] = t;
-console.log(a);
-console.log(b.innerText = [a]);
+// const b = document.createElement('span');
+// b.className = "tets";
+// b.style.fontWeight = '600';
+// b.style.fontSize = "25px"
+// document.body.append(b)
+// let a = [1, 2, 3, 4];
+// let t = a[0];
+// a[0] = a[a.length -1];
+// a[a.length -1] = t;
+// console.log(a);
+// console.log(b.innerText = [a]);
 
+// let out = '';
+// for(let i = 0; i < a.length; i++) {
+//   out += a[i] + '_ _';
 
+// }
+// document.querySelector('.out-1').innerHTML = out;
+// //_______________________________________________________
+// let y = [ 1111, 11233, 44444]
+// let out1 = '';
+// for(let i = 0; i < y.length; i++) {
+//   out1 += y[i] + '_ _';
 
-let out = '';
-for(let i = 0; i < a.length; i++) {
-  out += a[i] + '_ _';
+// }
+// document.querySelector('.out-2').innerHTML = out1;
 
+let b = [4, 39, 87, 99, 01, 129, 57];
+let max = b[0];
+for (let i = 0; i < b.length; i++)
+{
+  if (b[i] > max) {
+    max = b[i];
+  }
 }
-document.querySelector('.out-1').innerHTML = out;
+console.log("max: " + max);
