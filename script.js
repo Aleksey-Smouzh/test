7dc99c24a61cf39f0b19df52bd7c0fd2b56177b3
Aleksey-Smouzh <xmz@tut.by>
2021-12-12 21:33:18 +0300

    cycle p2/1

--- script.js
+++ script.js
@@ -167,9 +167,18 @@
 // }
 //document.querySelector('out-1').innerHTML = "out";
 const out = document.querySelector('.out')
-for (let i = 0; i < 5; i++){
-    for (let k = 0; k < 10; k++){
-      out.innerHTML += k;
-    }
-  out.innerHTML += " ";
+// for (let i = 0; i < 5; i++){
+//     for (let k = 0; k < 10; k++){
+//       out.innerHTML += k;
+//     }
+//   out.innerHTML += " ";
+// }
+for (let i = 0; i < 10; i++){
+  //out.innerHTML += "3*" + i + "=" + (i*3) + "<br>";
+// out.innerHTML +=`3*${i}=${3 * i}<br>`;
+
+for (let k = 0; k < 10; k++){
+  out.innerHTML +=`${i}*${k}=${k * i}<br>`;
+}
+out.innerHTML += '<hr>'
 }
